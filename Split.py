dafd23853772a95a81b4d1c93a22de4e329a8887
Vineetha Shaji <68177726+vineethashaji251998@users.noreply.github.com>
2023-05-27 14:44:34 +0530

    Update Split.py
    Split row into columns

--- Split.py
+++ Split.py
@@ -4,6 +4,6 @@ import pandas as pd
 data = pd.read_csv('Split.csv')
 df=pd.DataFrame(data)   
 df["Compound"] = df["Compound"].apply(lambda x: str(x).strip(';'))
-df["Compound"] = df["Compound"].str.split(";") 
+df["Compound"] = df["Compound"].str.split(";") # "Compound" is the name of the header
 df = df.explode("Compound").agg(pd.Series.tolist) 
 df.to_csv("output_File.csv", index=False)
